79c7aa576f6d9486f9cecd2db02491958c6fd5a6
Levi Shutts <lcs@uoregon.edu>
2018-10-09 10:36:33 -0600

    Almost fixed things

--- app/src/main/java/com/example/levi/wordz/MainActivity.java
+++ app/src/main/java/com/example/levi/wordz/MainActivity.java
@@ -4,22 +4,19 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.Button;
-import android.widget.TextView;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    private LinearLayout wordsToLookup;
-    private SharedPreferences.Editor editor;
-    private Set<String> restoredText;
+    private LinearLayout wordsDisplay;
+    private Set<String> savedWords;
+    private String savedNum;
 
     private int scrollViewCount = 0;
 
@@ -28,59 +25,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        Set<String> myWords = new HashSet<>();
+
+        System.out.println(myWords);
         //ScrollView with LinearLayout to put buttons
-        wordsToLookup = findViewById(R.id.wordsLayout);
-    }
-
-    void populateScroll(){
-        scrollViewCount = 0;
-        for(final String wordOrPhrase:restoredText){
-            Button newWordOrPhrase = new Button(this);
-            newWordOrPhrase.setOnClickListener( new View.OnClickListener() {
-
-                @Override
-                public void onClick(View v) {
-                    addDesActivity(wordOrPhrase, scrollViewCount);
-                }
-            });
-            newWordOrPhrase.setTransformationMethod(null);
-            newWordOrPhrase.setGravity(Gravity.LEFT);
-            newWordOrPhrase.setText(wordOrPhrase);
-
-            wordsToLookup.addView(newWordOrPhrase, scrollViewCount);
-            scrollViewCount += 1;
-        }
+        wordsDisplay = findViewById(R.id.wordsLayout);
+        savedWords = prefs.getStringSet("savedWords", myWords);
+        savedNum = "00000";
+        updateScrollView(savedWords);
     }
 
     //Called after word added or description added
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        System.out.println("Received a result from code " + requestCode);
         super.onActivityResult(requestCode, resultCode, data);
 
         //Add a button for a successful return from addWords
         if (requestCode == 1) {
             if(resultCode == RESULT_OK) {
-                final String wordOrPhrase = data.getStringExtra("wordOrPhrase");
-                Button newWordOrPhrase = new Button(this);
-                newWordOrPhrase.setOnClickListener( new View.OnClickListener() {
-
-                    @Override
-                    public void onClick(View v) {
-                        addDesActivity(wordOrPhrase, scrollViewCount);
-                    }
-                });
-                newWordOrPhrase.setTransformationMethod(null);
-                newWordOrPhrase.setGravity(Gravity.LEFT);
-                newWordOrPhrase.setText(wordOrPhrase);
-
-                wordsToLookup.addView(newWordOrPhrase, scrollViewCount);
-                scrollViewCount += 1;
+                String wordOrPhrase = savedNum + data.getStringExtra("wordOrPhrase");
+                updateSavedNum();
+                SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+                savedWords.add(wordOrPhrase);
+                editor.clear();
+                editor.putStringSet("savedWords", savedWords);
+                editor.commit();
             }
         }
 
         //Delete a button from MainActivity after successful delete or add description from addDescription
         if (requestCode == 2){
-            System.out.println("Back from addDescription");
             if(resultCode == RESULT_OK){
                 //TODO: return with word and scrollview value to remove from restoredText
                 //  clear, reset, and apply to editor
@@ -89,43 +63,64 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void updateSavedNum(){
+        Integer num = Integer.parseInt(savedNum);
+        num += 1;
+        savedNum = "0000" + num;
+    }
+
     @Override
     protected void onResume(){
         super.onResume();
-        //Recover buttons from String Set and repopulate wordsToLookup
+        //Recover buttons from String Set and repopulate wordsDisplay
         SharedPreferences prefs = getPreferences(MODE_PRIVATE);
-        if (prefs != null){
-            Set<String> myWords = new HashSet<>();
-            restoredText = prefs.getStringSet("List", myWords);
-            //TODO: sort by scrollViewCount
+        if(prefs != null){
+            prefs.getStringSet("savedWords", savedWords);
+            prefs.getString("savedNum", savedNum);
+            System.out.println("From onResume " + savedWords);
+            if(savedWords != null) {
+                updateScrollView(savedWords);
+            }
         }
-        populateScroll();
+    }
+
+    private void updateScrollView(Set<String> savedWords){
+        wordsDisplay.removeAllViews();
+        scrollViewCount = 0;
+        System.out.println("From updateScrollView " + savedWords);
+
+        for(final String wordOrPhrase:savedWords) {
+            Button newWordOrPhrase = new Button(this);
+            newWordOrPhrase.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    addDesActivity(wordOrPhrase, scrollViewCount);
+                }
+            });
+            newWordOrPhrase.setTransformationMethod(null);
+            newWordOrPhrase.setGravity(Gravity.LEFT);
+            newWordOrPhrase.setText(wordOrPhrase);
+            wordsDisplay.addView(newWordOrPhrase, scrollViewCount);
+            scrollViewCount += 1;
+        }
     }
 
     @Override
     protected void onPause(){
         super.onPause();
-
-        //Save buttons to String Set for saving ability
         SharedPreferences prefs = getPreferences(MODE_PRIVATE);
-        editor = prefs.edit();
-        //TODO: Create restoredText with map to keep scrollview number with button
-        //populate restoredText with button text and corresponding number
-        /*
-            View child = wordsToLookup.getChildAt(i);
-            myWords.add(((Button) child).getText().toString());
-        }
-        */
-        //Add restoredText to the editor
-        //editor.putStringSet("List", myWords);
+        SharedPreferences.Editor editor = prefs.edit();
+        System.out.println("From onPause " + savedWords);
+        editor.putStringSet("savedWords", savedWords);
+        editor.putString("savedNum", savedNum);
         editor.apply();
     }
 
     // Add button takes you to addWords activity
-    public void sendMessage(View v) {
+    public void addWord(View v) {
         Intent intent = new Intent(this, addWords.class);
 
-        //Return after Done and add button to wordsToLookup
+        //Return after Done and add button to wordsDisplay
         startActivityForResult(intent, 1);
     }
 
@@ -135,9 +130,8 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("info", word);
         intent.putExtra("button", button);
 
-        //Return after Done or Delete button are pressed to remove word/phrase from wordsToLookup
+        //Return after Done or Delete button are pressed to remove word/phrase from wordsDisplay
         //and Shared Preferences
-        System.out.println("Starting addDescription activity with intent to receive result");
         startActivityForResult(intent, 2);
     }
 }
